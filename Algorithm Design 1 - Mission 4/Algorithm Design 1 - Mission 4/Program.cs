4bc6fa9cde372b25b150e0f8f45f6ac2a181abc1
Sylwanas <alice@harlid.se>
2023-02-23 16:00:13 +0100

    Testicles

---
+++ Algorithm Design 1 - Mission 4/Algorithm Design 1 - Mission 4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm_Design_1___Mission_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
