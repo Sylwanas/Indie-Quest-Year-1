6714ce62e3116a7649699a9b9f230832cbb05fc9
Sylwanas <alice@harlid.se>
2023-04-05 14:22:47 +0200

    g

---
+++ Regex Boss Level/Regex Boss Level/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace Regex_Boss_Level
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] gameList = new[] { "https://store.steampowered.com/app/1811260/EA_SPORTS_FIFA_23/", "https://store.steampowered.com/app/1588610/Beers_and_Boomerangs/", "https://store.steampowered.com/app/2206340/Aokana__Four_Rhythms_Across_the_Blue__EXTRA2/", "https://store.steampowered.com/app/1173340/War_Trains/", "https://store.steampowered.com/app/1517290/Battlefield_2042/", "https://store.steampowered.com/app/1416420/Expansion__Europa_Universalis_IV_Leviathan/" };
+
+            for (int i = 0; i < gameList.Length; i++)
+            {
+                var httpClient = new HttpClient();
+                string htmlCode = httpClient.GetStringAsync($"{gameList[i]}").Result;
+
+                string findName = @"<title>(.*)\son\sSteam</title>";
+                string findRecent = @"<.*summary(?:positive|mixed)?.*?>(.*(Positive|Mixed|Negative))<";
+                string findOverall = @"<.*data-tooltip-html(?:positive|mixed)?.*?>(.*(Positive|Mixed|Negative))<";
+
+                Match matchName = Regex.Match(htmlCode, findName);
+                Match matchRecent = Regex.Match(htmlCode, findRecent);
+                Match matchOverall = Regex.Match(htmlCode, findOverall);
+
+                Console.WriteLine($"The reviews for {matchName.Groups[1].Value} are:");
+                Console.WriteLine($"Recent: {matchRecent.Groups[1].Value}");
+                Console.WriteLine($"Overall: {matchOverall.Groups[1].Value}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Regex/Regex/Program.cs
+++ Regex/Regex/Program.cs
@@ -1,48 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 
-namespace Regex
+namespace Regex_3____1
 {
-    public class Monster
-    {
-        public string name;
-        public bool fly;
-    }
     internal class Program
     {
-        static List<Monster> monsters = new List<Monster>();
         static void Main(string[] args)
         {
-            string[] regex = File.ReadAllLines("Regex.txt");
-            Monster monster = new Monster();
-            monster.name = regex[0];
-            monsters.Add(monster);
+            string[] regexArray = File.ReadAllLines("Regex.txt");
 
-            string speedPattern = @"fly [1-4]\d";
+            var namesByAlignment = new List<string>[3, 3];
+            var namesOfUnaligned = new List<string>();
+            var namesOfAnyAlignment = new List<string>();
+            var namesOfSpecialCases = new List<string>();
 
-            for (int i = 0; i < regex.Length; i++)
+            for (int axis1 = 0; axis1 < 3; axis1++)
             {
-                if (regex[i] == "")
+                for (int axis2 = 0; axis2 < 3; axis2++)
                 {
-                    monster = new Monster();
-                    monster.name = regex[i + 1];
-                    monsters.Add(monster);
-                    monster.fly = false;
-                }
-
-                if (System.Text.RegularExpressions.Regex.IsMatch(regex[i], speedPattern))
-                {
-                    monster.fly = true;
+                    namesByAlignment[axis1, axis2] = new List<string>();
                 }
             }
 
-            foreach (Monster creature in monsters)
+            string[] axis1Values = new[] { "chaotic", "neutral", "lawful" };
+            string[] axis2Values = new[] { "evil", "neutral", "good" };
+
+            for (int i = 0; i < regexArray.Length; i++)
             {
-                if (creature.fly == true)
+                Match match = Regex.Match(regexArray[i], @"((chaotic|neutral|lawful) (evil|neutral|good)|neutral)");
+                if (match.Success)
                 {
-                    Console.WriteLine($"{creature.name} - 10-49 flying speed: {creature.fly}");
+                    string monsterName = regexArray[i - 1];
+
+                    if (match.Groups[1].Value == "neutral")
+                    {
+                        namesByAlignment[1, 1].Add(monsterName);
+                    }
+                    else
+                    {
+                        string axis1Text = match.Groups[2].Value;
+                        string axis2Text = match.Groups[3].Value;
+                        int axis1 = Array.IndexOf(axis1Values, axis1Text);
+                        int axis2 = Array.IndexOf(axis2Values, axis2Text);
+                        namesByAlignment[axis1, axis2].Add(monsterName);
+                    }
                 }
+                else if (Regex.IsMatch(regexArray[i], @"unaligned"))
+                {
+                    namesOfUnaligned.Add(regexArray[i - 1]);
+                }
+                else if (Regex.IsMatch(regexArray[i], @"any alignment"))
+                {
+                    namesOfAnyAlignment.Add(regexArray[i - 1]);
+                }
+                else if (Regex.IsMatch(regexArray[i], @"any.*alignment"))
+                {
+                    namesOfSpecialCases.Add(regexArray[i - 1] + regexArray[i].Substring(regexArray[i].IndexOf(",")));
+                }
+            }
+
+            for (int axis1 = 0; axis1 < 3; axis1++)
+            {
+                for (int axis2 = 0; axis2 < 3; axis2++)
+                {
+                    string aligmentName = $"{axis1Values[axis1]} {axis2Values[axis2]}";
+
+                    if (aligmentName == "neutral neutral")
+                    {
+                        aligmentName = "true neutral";
+                    }
+                    Console.WriteLine($"{aligmentName} monsters:");
+                    foreach (string name in namesByAlignment[axis1, axis2])
+                    {
+                        Console.WriteLine(name);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("Unaligned monsters:");
+            foreach (string name in namesOfUnaligned)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Any aligned monsters:");
+            foreach (string name in namesOfAnyAlignment)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Special aligned monsters:");
+            foreach (string name in namesOfSpecialCases)
+            {
+                Console.WriteLine(name);
             }
         }
     }
