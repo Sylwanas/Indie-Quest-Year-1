8fb54a7390e98fa5d329d42547cac57522daf6fa
Sylwanas <alice@harlid.se>
2023-04-22 00:20:45 +0200

    Update Program.cs

--- Monster Manual 2/Monster Manual 2/Program.cs
+++ Monster Manual 2/Monster Manual 2/Program.cs
@@ -43,22 +43,84 @@ namespace Monster_Manual_1
             }
 
             Console.WriteLine("Welcome user, what monster would you like to be informed about?");
-            string monsterSearch = Console.ReadLine();
-
+            List<Monster> severalMonsters = new List<Monster>();
             Monster lookUpMonster = new Monster();
-
-            foreach (Monster monster in monsters)
+            bool foundMonster = false;
+            bool searching = true;
+            do
             {
-                if (monster.name.ToLower() == monsterSearch.ToLower())
+                string monsterSearch = Console.ReadLine();
+
+                foreach (Monster monster in monsters)
                 {
-                    lookUpMonster = monster;
-                    break;
+                    if (monster.name.ToLower().Contains(monsterSearch.ToLower()))
+                    {
+                        lookUpMonster = monster;
+                        severalMonsters.Add(monster);
+                        foundMonster = true;
+                    }
                 }
-            }
 
+                if (severalMonsters.Count == 1)
+                {
+                    foreach (Monster creature in severalMonsters)
+                    {
+                        Console.WriteLine($"Name: {creature.name}");
+                        Console.WriteLine($"Description: {creature.description}");
+                        Console.WriteLine($"Aligment: {creature.alignment}");
+                        Console.WriteLine($"Rolled HP: {creature.rollHP}");
+                    }
+                }
 
+                if (severalMonsters.Count > 1)
+                {
+                    Console.WriteLine($"I found several results containing {monsterSearch}. Which one would you like to know more about?");
+                    for (int i = 0; i < severalMonsters.Count; i++)
+                    {
+                        Console.WriteLine($"{i + 1}. {severalMonsters[i].name}");
+                    }
+                    string numberSearch = Console.ReadLine();
+                    int number = Convert.ToInt32(numberSearch);
+                    Console.WriteLine($"Name: {severalMonsters[number - 1].name}");
+                    Console.WriteLine($"Description: {severalMonsters[number - 1].description}");
+                    Console.WriteLine($"Aligment: {severalMonsters[number - 1].alignment}");
+                    Console.WriteLine($"Rolled HP: {severalMonsters[number - 1].rollHP}");
+                }
+
+                severalMonsters.Clear();
+
+                if (foundMonster == false)
+                {
+                    Console.WriteLine($"I could not any results containing {monsterSearch}, please try again.");
+                }
+                foundMonster = false;
+
+            } while (searching == true);
         }
     }
 }
+/*
+ *                     if (monster.name.ToLower().Contains(monsterSearch.ToLower()))
+                    {
+                        lookUpMonster = monster;
+                        severalMonsters.Add(monster);
 
+                        if (severalMonsters.Count > 1)
+                        {
+                            Console.WriteLine("There are several results user, which one would you like to know more about?");
+                            foreach (Monster creature in severalMonsters)
+                            {
+                                Console.WriteLine($"{creature.name}");
+                            }
+                            string monsterNumber = Console.ReadLine();
+                            int result = Convert.ToInt32(monsterNumber);
+                            Console.WriteLine($"Name: {monster.name[result - 1]}");
+                            Console.WriteLine($"Description: {monster.description[result - 1]}");
+                            Console.WriteLine($"Aligment: {monster.alignment[result - 1]}");
+                            Console.WriteLine($"Rolled HP: {monster.rollHP[result - 1]}");
+                            foundMonster = true;
+                            break;
+                        }
+                    }
 
+*/
